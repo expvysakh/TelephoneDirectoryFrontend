f64ee35537603c59e9ae613f4eb0d49c65bc8495
Raji Rajan <raji.rajan@experionglobal.com>
2017-02-02 14:54:10 +0530

    got username and password at login service.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import {Router,ActivatedRoute} from '@angular/router';
-import {NgForm} from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { LoginService } from './login.service';
+
 
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [LoginService]
 })
 export class LoginComponent implements OnInit {
+userName;
+Password;
+  constructor(private router: Router, private loginService: LoginService) {
 
-  constructor(private router: Router) { }
+   }
 
-  OnSignIn(form:NgForm){
-    
-  this.RedirectToHome();
-}
- 
+  OnSignIn(form: NgForm) {
+
+this.userName=form.value.username; 
+this.Password=form.value.password
+    this.RedirectToHome();
+
+   
+    this.loginService.Login(this.userName,this.Password);
+  
+  }
+
   RedirectToHome() {
     this.router.navigate(['/landing-container']);
   }
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -4,5 +4,9 @@ import { Injectable } from '@angular/core';
 export class LoginService {
 
   constructor() { }
+  Login(userName,Password){
+    console.log("From login service",userName,Password);
+
+  }
 
 }
